087c7d324b02e950ff9f3605889a7e0e6724125b
Stephen McIntosh <stephenmac7@gmail.com>
2012-08-01 16:47:23 -0400

    1.1

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ fi
 
 install -Dm755 desktop-creator.py ${DEST}usr/bin/desktop-creator
 install -Dm755 cli.py ${DEST}usr/bin/desktop-creator-cli
-install -Dm644 desktop_creator.ui ${DEST}usr/share/desktop-creator/desktop-creator.ui
+install -Dm644 desktop-creator.ui ${DEST}usr/share/desktop-creator/desktop-creator.ui
